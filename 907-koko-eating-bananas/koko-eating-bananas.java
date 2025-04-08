6f7973f6c83dd8dc968c08d2e589ea3a99e3831c
Rohit pandit <rohitpandit7061@gmail.com>
2025-04-08 14:36:29 +0530

    Time: 12 ms (74.23%) | Memory: 45.1 MB (76.41%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,22 +1,40 @@
 class Solution {
-  public int minEatingSpeed(int[] piles, int h) {
-    int l = 1;
-    int r = Arrays.stream(piles).max().getAsInt();
-
-    while (l < r) {
-      final int m = (l + r) / 2;
-      if (eatHours(piles, m) <= h)
-        r = m;
-      else
-        l = m + 1;
+  public int minEatingSpeed(int[] piles, int h){
+    int low=1;
+    int high=piles[0];
+    int ans=0;
+    for(int num:piles){
+        high=Math.max(num,high);
     }
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(IsItPossible(piles,mid,h)){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
 
-    return l;
   }
-
-  // Returns the hours to eat all the piles with speed m.
-  private int eatHours(int[] piles, int m) {
-    return Arrays.stream(piles).reduce(
-        0, (subtotal, pile) -> subtotal + (pile - 1) / m + 1); // ceil(pile / m)
-  }
+  public boolean IsItPossible(int piles[],int mid,int totalhour){
+    int h=0;
+    for(int i=0;i<piles.length;i++){
+        if(piles[i]<=mid){
+            h++;
+        }
+        else if(piles[i]%mid==0){
+            h=h+piles[i]/mid;
+        }
+        else{
+            h=h+piles[i]/mid+1;
+        }
+        if(h>totalhour){
+            return false;
+        }
+    }
+    return true;
+  }  
 }
