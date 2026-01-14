05a062b83407910b99a264541dbccdb46612505d
Rohit pandit <rohitpandit7061@gmail.com>
2026-01-15 00:16:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (29.82%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int [][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return solve(0,0,m,n,dp,obstacleGrid);
+    }
+    public int solve(int i, int j, int m, int n, int [][] dp,int[][] obstacleGrid){
+        if(i>=m || j>=n || obstacleGrid[i][j]==1){
+            return 0;
+        }
+        if (i == m - 1 && j == n - 1) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int right = solve(i,j+1,m,n,dp,obstacleGrid);
+        int down = solve(i+1,j,m,n,dp,obstacleGrid);
+        return dp[i][j] = right + down;
+    }
+}
