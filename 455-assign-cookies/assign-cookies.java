1d5a14b6078a2933745c91ee9bda4950329dd29d
Rohit pandit <rohitpandit7061@gmail.com>
2026-05-05 15:18:06 +0530

    Time: 15 ms (64.59%) | Memory: 51.4 MB (73.82%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        int p1=0,p2=0;
+        while(p1<g.length && p2<s.length){
+            if(s[p2]>=g[p1]){
+                count++;
+                p1++;
+                p2++;
+            }else if (s[p2]<g[p1]){
+                p2++;
+            }
+        }
+        return count;
+    }
+}
