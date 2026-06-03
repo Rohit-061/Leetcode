821205beacbfc00be6fc326308756b65a4006915
Rohit pandit <rohitpandit7061@gmail.com>
2026-06-03 22:44:19 +0530

    Time: 2 ms (99.51%) | Memory: 53.6 MB (48.95%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int len = trust.length;
+        if(len<n-1){
+            return -1;
+
+        }
+        int[] in = new int[n+1];
+        int[] out = new int[n+1];
+        for(int i=0;i<len;i++){
+            out[trust[i][0]]++;
+            in[trust[i][1]]++;
+        }
+        for(int i=1;i<n+1;i++){
+            if(out[i]==0 && in[i]==n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
