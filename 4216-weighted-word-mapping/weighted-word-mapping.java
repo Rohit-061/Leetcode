89f7e6678b0e8da2ed3a110fa92d0cd4e69e09d5
Rohit pandit <rohitpandit7061@gmail.com>
2026-06-20 21:43:43 +0530

    Time: 2 ms (93.19%) | Memory: 46.7 MB (17.35%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder result = new StringBuilder();
+        
+        for (String word : words) {
+            int sum = 0;
+            for (char c : word.toCharArray()) {
+                sum += weights[c - 'a']; // add weight of each character
+            }
+            
+            int mod = sum % 26;
+            char mapped = (char) ('z' - mod); // reverse alphabetical mapping
+            result.append(mapped);
+        }
+        
+        return result.toString();
+    }
+}
