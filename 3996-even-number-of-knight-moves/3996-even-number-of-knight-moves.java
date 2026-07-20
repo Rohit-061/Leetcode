dc0ac13ce30d5d73070522897e758646322e32f4
Rohit pandit <rohitpandit7061@gmail.com>
2026-07-20 22:31:18 +0530

    Time: 0 ms (100%), Space: 43.8 MB (100%) - LeetHub

---
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean canReach(int[] start, int[] target) {
+        int p1 = (start[0]+start[1])%2;
+        int p2 = (target[0]+target[1])%2;
+        return p1 == p2;
+    }
+}
