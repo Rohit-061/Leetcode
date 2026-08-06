a3ba3cd49a709236ca6a4eba1ca3480871f7c63a
Rohit pandit <rohitpandit7061@gmail.com>
2026-08-06 12:04:00 +0530

    Time: 629 ms (18.38%), Space: 43.2 MB (70.09%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    int n;
+    public int findTargetSumWays(int[] nums, int target) {
+        n = nums.length;
+        return solve(nums,0,0,target);
+        
+    }
+    public int solve(int[] nums,int i,int sum,int target){
+        if(i == n){
+            if(sum == target) return 1;
+            else return 0;
+        }
+      
+        int add = solve(nums,i+1,sum + nums[i],target);
+        int sub = solve(nums,i+1,sum-nums[i] ,target);
+        return add+sub;
+
+    }
+}
