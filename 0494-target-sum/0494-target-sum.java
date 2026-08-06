f7c2f6e08d765a58448bb35714059827f2c43a26
Rohit pandit <rohitpandit7061@gmail.com>
2026-08-06 13:06:23 +0530

    Time: 8 ms (60.51%), Space: 46.9 MB (22.84%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,28 @@
 class Solution {
-    int n;
+    int n,s;
     public int findTargetSumWays(int[] nums, int target) {
         n = nums.length;
-        return solve(nums,0,0,target);
+        s = 0;
+        for(int num: nums){
+            s += num;
+        }
+        int[][] dp = new int[n+1][2*s+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(nums,0,0,target,dp);
         
     }
-    public int solve(int[] nums,int i,int sum,int target){
+    public int solve(int[] nums,int i,int sum,int target,int[][] dp){
         if(i == n){
             if(sum == target) return 1;
             else return 0;
         }
-      
-        int add = solve(nums,i+1,sum + nums[i],target);
-        int sub = solve(nums,i+1,sum-nums[i] ,target);
-        return add+sub;
+        if(dp[i][sum+s]!=-1) return dp[i][sum+s];
+        int add = solve(nums,i+1,sum + nums[i],target,dp);
+        int sub = solve(nums,i+1,sum-nums[i] ,target,dp);
+        dp[i][sum+s] = add+sub;
+        return dp[i][sum+s];
 
     }
 }
