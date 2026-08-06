2b834a7d1ee3642afbf8aeaf90c90bb782387a3b
Rohit pandit <rohitpandit7061@gmail.com>
2026-08-06 20:40:13 +0530

    Time: 390 ms (9.59%), Space: 237.5 MB (5.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,25 +1,23 @@
 class Solution {
+    int n;
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int [n][n];
+        n = nums.length;
+        int[][] dp = new int[n+1][n+1];
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
+            Arrays.fill(dp[i],-1);
         }
         return solve(nums,0,-1,dp);
     }
-    public int solve(int[] nums,int index,int prev,int[][] dp){
-        int n = nums.length;
-        if(index==n) return 0;
-        if(dp[index][prev+1]!=-1) return dp[index][prev+1];
-        int pick =0;
-        if(prev ==-1 || nums[index]>nums[prev]){
-            pick= 1 + solve(nums,index+1,index,dp);
+    public int solve(int[] nums,int i,int prev,int[][] dp){
+        if(i==n) return 0;
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        int pick=0;
+        if((prev == -1) || nums[i]>nums[prev]){
+            pick = 1 + solve(nums,i+1,i,dp);
         }
-        int nopick = solve(nums,index+1,prev,dp);
-        return dp[index][prev+1] = Math.max(pick,nopick);
-        
-        
+        int nopick = solve(nums,i+1,prev,dp);
+        dp[i][prev+1] = Math.max(pick,nopick);
+        return dp[i][prev+1];
+
     }
 }
