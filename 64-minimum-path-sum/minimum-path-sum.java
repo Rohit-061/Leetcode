cc1057bfb27ec7fdf1409f5984ca9e88e56f09f7
Rohit pandit <rohitpandit7061@gmail.com>
2026-08-10 23:54:46 +0530

    Time: 1 ms (99.89%) | Memory: 50 MB (85.87%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,7 +1,8 @@
 class Solution {
+    int n,m;
     public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        n = grid.length;
+        m = grid[0].length;
         int[][] dp = new int[n+1][m+1];
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
@@ -9,8 +10,6 @@ class Solution {
         return solve(0,0,grid,dp);
     }
     public int solve(int i,int j,int[][] grid,int[][] dp){
-        int n = grid.length;
-        int m = grid[0].length;
         if(i==n-1 && j==m-1){
             dp[i][j] = grid[i][j]; 
             return grid[i][j];
