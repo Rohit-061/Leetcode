2713b2062a46828113862705552ae5981100cdd0
Rohit pandit <rohitpandit7061@gmail.com>
2026-08-15 21:28:41 +0530

    Time: 1 ms (97.17%), Space: 58.4 MB (30.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cs
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -3,11 +3,13 @@ public class Solution {
         int pos = 0;
         for(int i=0;i<nums.Length;i++){
             if(nums[i]!=0){
-                nums[pos++] = nums[i];
+                nums[pos] = nums[i];
+                pos++;
             }
         }
         while(pos<nums.Length){
-            nums[pos++]=0;
+            nums[pos]=0;
+            pos++;
         }
     }
 }
